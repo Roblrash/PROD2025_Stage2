1fa46c8ea285abd89f02bb4a1ed2e5977c98ee23
Roblrash <mr.roman.elistratov@mail.ru>
2025-01-20 02:25:40 +0300

    fix entry.sh

--- solution/entry.sh
+++ solution/entry.sh
@@ -1,5 +1,5 @@
 alembic -c solution/alembic.ini upgrade head
-exec gunicorn main:app \
+gunicorn main:app \
   --worker-class uvicorn.workers.UvicornWorker \
   --bind "$SERVER_ADDRESS:$SERVER_PORT" \
   --access-logfile -
