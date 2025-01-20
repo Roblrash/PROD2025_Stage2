5a8bb3f496b13ffae5b8950baf938bfad43711c5
Roblrash <mr.roman.elistratov@mail.ru>
2025-01-20 04:16:23 +0300

    migration

--- solution/entry.sh
+++ solution/entry.sh
@@ -1,4 +1,4 @@
-alembic -c solution/alembic.ini upgrade head
+alembic -c solution/alembic.ini upgrade 27af32b57c93
 gunicorn main:app \
   --worker-class uvicorn.workers.UvicornWorker \
   --bind "$SERVER_ADDRESS:$SERVER_PORT" \
